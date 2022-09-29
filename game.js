9a6c701b2d8a572146a55f745a492bb938d13dc1
Mary <mcrowe0331@gmail.com>
2022-09-28 23:57:55 -0500

    Added end.js and end.html content

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -24,7 +24,7 @@ let questions = [
     choice1: "1904",
     choice2: "1926",
     choice3: "1948",
-    choice4:"1969",
+    choice4: "1969",
     answer: 2
   },
   {
@@ -51,9 +51,9 @@ let questions = [
       choice4: "Greek",
       answer: 1
     },
-];
+  ];
 
-//Constants
+//CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 5;
 
@@ -61,42 +61,44 @@ startGame = () => {
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
-  console.log(availableQuestions);
   getNewQuestion();
 };
 
 getNewQuestion = () => {
-  if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    // go to the end page
-    return window.location.assign('/end.html');
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    //go to the end page
+    return window.location.assign("/end.html");
   }
+
   questionCounter++;
   progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-  //Update the progress bar
+  //update progress bar
   progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-  currentQuestion = availableQuestions[questionIndex];
-  question.innerText = currentQuestion.question;
 
-  choices.forEach(choice => {
-    const number = choice.dataset["number"];
-    choice.innerText = currentQuestion["choice" + number];
-  });
+ const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+ currentQuestion = availableQuestions[questionIndex];
+ question.innerText = currentQuestion.question;
 
+ choices.forEach(choice => {
+  const number = choice.dataset["number"];
+  choice.innerText = currentQuestion["choice" + number];
+});
 
 availableQuestions.splice(questionIndex, 1);
-acceptingAnswers = true;
+  acceptingAnswers = true;
 };
 
 choices.forEach(choice => {
   choice.addEventListener("click", e => {
-    if(!acceptingAnswers) return;
+    if (!acceptingAnswers) return;
 
     acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
 
-    const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+    const classToApply =
+    selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
     if (classToApply === "correct") {
       incrementScore(CORRECT_BONUS);
@@ -104,9 +106,9 @@ choices.forEach(choice => {
 
     selectedChoice.parentElement.classList.add(classToApply);
 
-    setTimeout( () => {
-    selectedChoice.parentElement.classList.remove(classToApply);
-    getNewQuestion();
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
     }, 1000);
   });
 });
@@ -115,5 +117,6 @@ incrementScore = num => {
   score += num;
   scoreText.innerText = score;
 };
-
 startGame();
+
+  
